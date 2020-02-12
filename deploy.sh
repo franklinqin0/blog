2da3f1c3fddb5e975fa69fe4434d90826d778742
Franklin Qin <franklinqin0@gmail.com>
2020-02-12 22:49:31 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:franklinqin0/franklinqin0.github.io.git gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:franklinqin0/franklinqin0.github.io.git master:gh-pages
+# git push -f git@github.com:franklinqin0/franklinqin0.github.io.git master:gh-pages
 
 cd -
