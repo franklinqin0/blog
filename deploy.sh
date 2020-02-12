cb89f400aff0eb8639c580d042219950f41c6216
Franklin Qin <franklinqin0@gmail.com>
2020-02-12 22:48:34 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 # build
-npm install && npm run build
+npm run docs:build
 
 # navigate into the build output directory
-cd public
+cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -15,6 +15,11 @@ cd public
 git init
 git add -A
 git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:franklinqin0/franklinqin0.github.io.git master:gh-pages
 
 cd -
