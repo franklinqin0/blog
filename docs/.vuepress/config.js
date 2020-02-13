0782d25ec2056301c2fa7f80d9cec46ef0074715
franklinqin0 <franklinqin0@gmail.com>
2020-02-13 13:09:48 +0800

    changed build dest path

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ const themeConfig = require('./config/theme/')
 module.exports = {
   title: "Franklin Qin",
   description: '哲人日已遠，典型在夙昔。風簷展書讀，古道照顏色',
-  dest: 'public',
+  dest: 'docs/.vuepress/dist',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
