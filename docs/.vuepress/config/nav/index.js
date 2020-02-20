e80c31c6724bc7e65cb8109da4655e02e32bf487
franklinqin0 <franklinqin0@gmail.com>
2020-02-20 17:49:39 +0800

    add wc and fb contacts

--- docs/.vuepress/config/nav/index.js
+++ docs/.vuepress/config/nav/index.js
@@ -5,9 +5,10 @@ module.exports = [
     icon: 'reco-message',
     items: [
       { text: 'Email', link: 'mailto:franklinqin0@gmail.com?subject=I read your blog and here is what I think', icon: 'reco-mail' },
+      { text: 'Wechat', link: 'https://franklinqin0.github.io/blog/wechat.png', icon: 'reco-wechat'},
+      { text: 'Facebook', link: 'https://www.facebook.com/franklinqin0', icon: 'reco-facebook'},
       { text: 'LinkedIn', link: 'https://www.linkedin.com/in/franklinqin0/', icon: 'reco-linkedin' },
       { text: 'GitHub', link: 'https://github.com/franklinqin0', icon: 'reco-github' },
-      // { text: 'WeChat', link: 'yuanwelldone', icon: 'reco-wechat' },
     ]
   }
 ]
