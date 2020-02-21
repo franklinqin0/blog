a60e24564c7f3dcf15a670e0ee4d0620adde16cc
franklinqin0 <franklinqin0@gmail.com>
2020-02-21 15:32:54 +0800

    changed contact to more

--- docs/.vuepress/config/nav/index.js
+++ docs/.vuepress/config/nav/index.js
@@ -1,15 +1,15 @@
 module.exports = [
   { text: 'Home', link: '/', icon: 'reco-home' },
   { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
-  { text: 'Contact', 
+  { text: 'More', 
     icon: 'reco-message',
     items: [
-      { text: 'Subscribe vis RSS', link: 'https://franklinqin0.github.io/blog/rss', icon: 'reco-rss' },
+      { text: 'RSS', link: 'https://franklinqin0.github.io/blog/rss', icon: 'reco-rss' },
       { text: 'Email', link: 'mailto:franklinqin0@gmail.com?subject=I read your blog and here is what I think', icon: 'reco-mail' },
+      { text: 'GitHub', link: 'https://github.com/franklinqin0', icon: 'reco-github' },
       { text: 'Wechat', link: 'https://franklinqin0.github.io/blog/wechat.png', icon: 'reco-wechat'},
       { text: 'Facebook', link: 'https://www.facebook.com/franklinqin0', icon: 'reco-facebook'},
       { text: 'LinkedIn', link: 'https://www.linkedin.com/in/franklinqin0/', icon: 'reco-linkedin' },
-      { text: 'GitHub', link: 'https://github.com/franklinqin0', icon: 'reco-github' },
     ]
   }
 ]
